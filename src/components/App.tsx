bfa0ff6bf9cb8b76431d47b4cc0667972a73e5db
Yuki Takenaka <yuki.takenaka@andco.group>
2020-01-08 14:18:17 +0900

    refでの操作

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'stores';
@@ -7,22 +7,39 @@ import userSlice from 'stores/user';
 export default () => {
   const dispatch = useDispatch();
   const name = useSelector((state: RootState) => state.user.name);
+  const input = useRef<HTMLInputElement>(null);
 
   const onTextChange = (value: string) => {
     dispatch(userSlice.actions.setName(value));
+  };
+
+  const onButtonClicked = () => {
+    if (input.current === null) {
+      return;
+    }
+
+    dispatch(userSlice.actions.clearName());
+    input.current.value = '';
   };
 
   return (
     <App>
       <h1>{name}</h1>
 
-      <input type="text" onChange={e => onTextChange(e.target.value)} />
+      <input
+        ref={input}
+        type="text"
+        onChange={e => onTextChange(e.target.value)}
+      />
+
+      <button onClick={onButtonClicked}>clear</button>
     </App>
   );
 };
 
 const App = styled.div`
   h1 {
-    color: red;
+    color: gray;
+    height: 2rem;
   }
 `;
