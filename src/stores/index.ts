db668a3398dfc5a2b88bea7a808a0a00ba92ee0b
Yuki Takenaka <yuki.takenaka@andco.group>
2020-01-09 14:42:58 +0900

    add AppThunk

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,5 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-
+import { configureStore, combineReducers, Action } from '@reduxjs/toolkit';
+import { ThunkAction } from 'redux-thunk';
 import userSlice from 'stores/user';
 
 const reducer = combineReducers({
@@ -9,5 +9,6 @@ const reducer = combineReducers({
 const store = configureStore({ reducer });
 
 export type RootState = ReturnType<typeof reducer>;
+export type AppThunk = ThunkAction<void, RootState, null, Action<string>>;
 
 export default store;
